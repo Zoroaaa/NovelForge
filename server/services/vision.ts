5047fd38b9761ddc350dfc0a8651663d9ca1416b
unknown <1121896694@qq.com>
2026-04-22 14:04:16 +0800

    强化

--- server/services/vision.ts
+++ server/services/vision.ts
@@ -223,12 +223,7 @@ function parseVisionResponse(
  * File/Blob 转 ArrayBuffer
  */
 async function fileToArrayBuffer(file: File | Blob): Promise<ArrayBuffer> {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader()
-    reader.onloadend = () => resolve(reader.result as ArrayBuffer)
-    reader.onerror = reject
-    reader.readAsArrayBuffer(file)
-  })
+  return file.arrayBuffer()
 }
 
 /**
